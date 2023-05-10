65559a53694edd85761e8096839dac17ef68e38b
kizaru <rizkiramada128@gmail.com>
2023-05-10 16:14:42 +0700

    membuat class pelajar dengan dan memberikan nilai pada "nilai" =100

--- friendclass3/friendclass3.cpp
+++ friendclass3/friendclass3.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 class pelajar;
 
+class manusia {
+public:
+    void showNilaiPelajar(pelajar&);
+};
+
+class pelajar {
+private:
+    int nilai;
+public:
+    pelajar() {
+        nilai = 100;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
